54d97b2d0127eca5ea8a7a163ea42bdc9014dc36
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2024-02-22 21:54:37 -0300

    boton y funcionalidad de show

--- src/components/pokemons/pokemon-image.tsx
+++ src/components/pokemons/pokemon-image.tsx
@@ -3,13 +3,14 @@ import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 interface Props {
     id: number,
     size?: number,
-    backImage: boolean
+    backImage: boolean,
+    isVisible: boolean, 
 };
 
 
-export const PokemonImage = component$(({id, size, backImage= false} : Props) => {
+export const PokemonImage = component$(({id, size, backImage= false, isVisible=false} : Props) => {
 
-    let imageLoaded = useSignal(true);
+    let imageLoaded = useSignal(false);
 
     //es un hook para disparar efectos secundarios  
     // en este caso actua mientras el id cambia 
@@ -24,13 +25,20 @@ export const PokemonImage = component$(({id, size, backImage= false} : Props) =>
         imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`
     }; 
 
+    // if(isVisible ) {
+    //     imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+    // }
+
     return(
         <div class='flex items-center justify-center' style={{width: size, height: size}}>
          {!imageLoaded.value  &&  <span class='text-purple-300 text-base'>Loading...</span>}
          <img width="96" height="96" src={imageUrl}
            style={{ width: size}}
            onLoad$={ () => imageLoaded.value = true}
-           class={{'hidden' : !imageLoaded.value}}>
+           class={[{ 
+            'hidden' : !imageLoaded.value,
+            'brightness-0': isVisible
+           }, 'transition-all']}>
          </img>
         </div>
     )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,6 +12,7 @@ export default component$(() => {
 // useSignal para valores primitivos como booleans, strings, numeros
 const pokemonId = useSignal(1); 
 const showBackImage = useSignal(false);
+const isVisible = useSignal(false); 
 
 
 const changePokemonId = $(( value: number ) => {
@@ -30,12 +31,12 @@ const changePokemonId = $(( value: number ) => {
         <span class='text-2xl'>Simple search </span>
         <span class='text-2xl'>{pokemonId}</span>
       </div>
-      <PokemonImage id={pokemonId.value} size={250} backImage={showBackImage.value}/>
+      <PokemonImage id={pokemonId.value} size={250} backImage={showBackImage.value} isVisible={isVisible.value}/>
       <div class='mt-2'>
         <button onClick$={ () => changePokemonId(-1) } class='btn btn-primary mr-2'>Previous</button>
         <button onClick$={ () => changePokemonId(+1) } class='btn btn-primary mr-2'>Next</button>
-        <button onClick$={ () => showBackImage.value = !showBackImage.value } class='btn btn-primary'>Rotate</button>
-
+        <button onClick$={ () => showBackImage.value = !showBackImage.value } class='btn btn-primary mr-2'>Rotate</button>
+        <button onClick$={ () => isVisible.value = !isVisible.value } class='btn btn-primary mr-2'>Show</button>
       </div>
      
     </>
